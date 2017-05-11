39ac55d0f78add867f90a003d1912793b90af57e
Maria Sukhareva <sukhareva@ukp.informatik.tu-darmstadt.de>
2017-05-11 16:49:36 +0200

    changed the parameters, added bidirectional layer

--- tagger/lstm_model.py
+++ tagger/lstm_model.py
@@ -6,6 +6,10 @@ from keras.preprocessing.sequence import pad_sequences
 from keras.layers.embeddings import Embedding
 from sklearn.metrics import confusion_matrix, accuracy_score, precision_recall_fscore_support
 from sklearn.model_selection import train_test_split
+from keras.layers import Dense, Dropout, Embedding, LSTM, Bidirectional
+from keras.backend import tf
+from lambdawithmask import Lambda as MaskLambda
+from keras.layers import Merge
 
 TRAIN_FILE = "../data/train_small.txt"
 
@@ -25,15 +29,17 @@ all_x = all_x[:-1]
 lengths = [len(x) for x in all_x]
 print 'Input sequence length range: ', max(lengths), min(lengths)
 
-X = [[c[0] for c in x] for x in all_x]
-Y = [[c[1] for c in y] for y in all_x]
+short_x = [x for x in all_x if len(x) < 64]
+
+X = [[c[0] for c in x] for x in short_x]
+y = [[c[1] for c in y] for y in short_x]
 
 all_text = [c for x in X for c in x]
 
 words = list(set(all_text))
 word2ind = {word: index for index, word in enumerate(words)}
 ind2word = {index: word for index, word in enumerate(words)}
-labels = list(set([c for x in Y for c in x]))
+labels = list(set([c for x in y for c in x]))
 label2ind = {label: (index + 1) for index, label in enumerate(labels)}
 ind2label = {(index + 1): label for index, label in enumerate(labels)}
 print 'Vocabulary size:', len(word2ind), len(label2ind)
@@ -41,16 +47,15 @@ print 'Vocabulary size:', len(word2ind), len(label2ind)
 maxlen = max([len(x) for x in X])
 print 'Maximum sequence length:', maxlen
 
-X_enc = [[word2ind[c] for c in x] for x in X]
-
-max_label = max(label2ind.values()) + 1
 
 def encode(x, n):
     result = np.zeros(n)
     result[x] = 1
     return result
 
-y_enc = [[0] * (maxlen - len(ey)) + [label2ind[c] for c in ey] for ey in Y] #ey - sequence output in Y, c is a label in the sequence
+X_enc = [[word2ind[c] for c in x] for x in X]
+max_label = max(label2ind.values()) + 1
+y_enc = [[0] * (maxlen - len(ey)) + [label2ind[c] for c in ey] for ey in y]
 y_enc = [[encode(c, max_label) for c in ey] for ey in y_enc]
 
 X_enc = pad_sequences(X_enc, maxlen=maxlen)
@@ -60,22 +65,23 @@ X_train, X_test, y_train, y_test = train_test_split(X_enc, y_enc, test_size=4*32
 print 'Training and testing tensor shapes:', X_train.shape, X_test.shape, y_train.shape, y_test.shape
 
 max_features = len(word2ind)
-embedding_size = 500
-hidden_size = 128
+embedding_size = 650
+hidden_size = 150
 out_size = len(label2ind) + 1
 
 model = Sequential()
 model.add(Embedding(max_features, embedding_size, input_length=maxlen, mask_zero=True))
-model.add(LSTM(hidden_size, return_sequences=True))
+model.add(Bidirectional(LSTM(hidden_size, return_sequences=True)))
 model.add(TimeDistributedDense(out_size))
 model.add(Activation('softmax'))
 
-model.compile(loss='categorical_crossentropy', optimizer='adam')
+model.compile(loss='binary_crossentropy', optimizer='adam')
 
 batch_size = 32
 model.fit(X_train, y_train, batch_size=batch_size, nb_epoch=10, validation_data=(X_test, y_test))
 score = model.evaluate(X_test, y_test, batch_size=batch_size)
 print('Raw test score:', score)
+
 
 def score(yh, pr):
     coords = [np.where(yhh > 0)[0][0] for yhh in yh]
